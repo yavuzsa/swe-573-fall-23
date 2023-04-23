4160856dd53666377fe46e3a552e30ce8be0b139
yavuzsa <slhyvz7@gmail.com>
2023-04-23 06:11:59 +0300

    Initial commit

---
+++ src/main/java/com/project/storyapp/StoryappApplication.java
@@ -0,0 +1,13 @@
+package com.project.storyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryappApplication.class, args);
+	}
+
+}
