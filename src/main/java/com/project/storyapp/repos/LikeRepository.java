d5b837ffb94a51e36294bc7a2bc918eafaed4fd0
yavuzsa <slhyvz7@gmail.com>
2023-05-02 02:06:19 +0300

    Implement like service

--- src/main/java/com/project/storyapp/repos/LikeRepository.java
+++ src/main/java/com/project/storyapp/repos/LikeRepository.java
@@ -3,6 +3,13 @@ package com.project.storyapp.repos;
 import com.project.storyapp.entities.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LikeRepository extends JpaRepository<Like, Long> {
 
+    List<Like> findByUserIdAndPostId(Long userId, Long postId);
+
+    List<Like> findByUserId(Long userId);
+
+    List<Like> findByPostId(Long postId);
 }
