1e7852dba11e2b4662c54b0ff27a95c46c0aab8b
yavuzsa <slhyvz7@gmail.com>
2023-05-02 02:05:50 +0300

    Implement comment service

--- src/main/java/com/project/storyapp/repos/CommentRepository.java
+++ src/main/java/com/project/storyapp/repos/CommentRepository.java
@@ -3,6 +3,13 @@ package com.project.storyapp.repos;
 import com.project.storyapp.entities.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+    List<Comment> findByUserIdAndPostId(Long userId, Long postId);
+
+    List<Comment> findByUserId(Long userId);
+
+    List<Comment> findByPostId(Long postId);
 }
---
+++ src/main/java/com/project/storyapp/requests/CommentUpdateRequest.java
@@ -0,0 +1,9 @@
+package com.project.storyapp.requests;
+
+import lombok.Data;
+
+@Data
+public class CommentUpdateRequest {
+
+    String text;
+}
